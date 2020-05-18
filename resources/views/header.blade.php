dde8ec018daf165e166b413a5640b35eb9bb8e3f
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-05-18 15:06:20 +0200

    Add header page

---
+++ resources/views/header.blade.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link
+            href="https://fonts.googleapis.com/css?family=Nunito:200,600"
+            rel="stylesheet"
+        />
+
+        <!-- Styles -->
+        <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+        <link
+            rel="stylesheet"
+            href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+            integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
+            crossorigin="anonymous"
+        />
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    </head>
+    <body>
+        <div class="header">
+            <div class="top-left">
+                <img class="logo" src="images/logo.png" alt="logo" />
+            </div>
+
+            <div class="top-center">
+                <form action="" class="d-flex mr-3">
+                    <input
+                        type="text"
+                        name="q"
+                        placeholder="Trouvez votre jeu !"
+                    />
+                    <button type="submit" class="btn btn-info">
+                        <i class="fas fa-search"></i>
+                    </button>
+                </form>
+            </div>
+
+            @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                <a href="{{ url('/home') }}">Mon compte</a>
+                @else
+                <a href="{{ route('login') }}">Login</a>
+                @if (Route::has('register'))
+                <a href="{{ route('register') }}">Register</a>
+                @endif @endauth
+            </div>
+            @endif
+        </div>
+    </body>
+</html>
