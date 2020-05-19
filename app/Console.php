d578708badd2a75b981695d1aa4043aa82bea50e
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-05-19 23:01:33 +0200

    Add console Model

---
+++ app/Console.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Console extends Model
+{
+    protected $table="consoles";
+
+    protected $fillable=[
+        'id', 
+        'console'
+    ];
+}
