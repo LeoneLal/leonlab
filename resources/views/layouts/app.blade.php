bfae5b708376431392aad1b2e39bbce32abf972c
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-05-28 12:14:39 +0200

    Add route to admin panel

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -62,6 +62,9 @@
                                         {{ __('Logout') }}
                                     </a>
 
+                                    <a class="dropdown-item" href="{{ route('jeux.create') }}">{{ __('Admin') }}</a>
+
+
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
