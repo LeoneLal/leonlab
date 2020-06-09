c4f8c78c503744eaff323f6cf100a693857f02b4
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-09 10:12:58 +0200

    Add destroy game in cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -51,4 +51,18 @@ class CartController extends Controller
         
     }
 
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+
+        return back()->with('success', 'Le produit a été supprimé.');
+    }
+
 }
