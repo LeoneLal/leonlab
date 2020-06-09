e3cec30b302103327544738a846911eb844453b8
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-09 10:11:45 +0200

    Create CartController

---
+++ app/Http/Controllers/CartController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Http\Request;
+use App\Fiche;
+use App\Jeu;
+use App\Ligne;
+use App\User;
+
+
+class CartController extends Controller
+{
+}
