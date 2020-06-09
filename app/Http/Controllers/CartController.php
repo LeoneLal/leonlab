5d969348c1411424a9293136bb8ba4e33f9caea2
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-09 10:12:42 +0200

    Add display cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,4 +12,43 @@ use App\User;
 
 class CartController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $user = \Auth::user();
+        return view('cart.index', compact('user'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $duplicata = Cart::search(function ($cartItem, $rowId) use($request){
+            return $cartItem->id === $request->product_id;
+        });
+
+        if($duplicata->isNotEmpty()) {
+            return redirect()->route('jeux.index')->with('warning', 'Le jeu est déjà dans le panier !');
+        }
+        
+        $product = Jeu::find($request->product_id);
+
+        Cart::add($product->id, $product->nom, 1, $product->prix)
+            ->associate('App\Jeu');
+
+        
+
+        return redirect()->route('jeux.index')->with('success', 'Le jeu est ajouté au panier !');
+        
+    }
+
 }
