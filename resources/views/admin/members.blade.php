033887bf5ee2589f6c436ee1d9d5bc0dc5acf0a3
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-10 19:20:49 +0200

    Create admin members view

---
+++ resources/views/admin/members.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
+    <link href="{{ asset('css/admin.css') }}" rel="stylesheet" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="header">
+      <div class="top-left">
+        <a href="{{ url('/') }}" class="link"><img class="logo" src="{{ asset('images/logo.png') }}" alt="logo" /></a>
+      </div>
+
+      <div class="top-center">
+        <form method='GET' action="{{ route('jeux.search')}}" class="d-flex mr-3">
+            <input type="text" name="q" placeholder="Trouvez votre jeu !" />
+            <button type="submit" class="btn btn-info">
+                <i class="fas fa-search"></i>
+            </button>
+        </form>
+      </div>
+
+      @if (Route::has('login'))
+      <div class="top-right links">
+        @auth
+        <a href="{{ url('/home') }}">Mon compte</a>
+        @else
+        <a href="{{ route('login') }}">Login</a>
+        @if (Route::has('register'))
+        <a href="{{ route('register') }}">Register</a>
+        @endif @endauth
+      </div>
+      @endif
+    </div>
+    <h2 class="title" >Administration</h2>
+    <nav class="navbarre">
+        <a href="{{route('admin.index')}}">
+            <div class="actual">
+                <p>Tableau de bord</p>
+                <div class="on"></div>
+            </div>
+        </a>
+        <a href="{{route('admin.game')}}">
+            <div>
+                <p>Jeux</p>
+                <div class="on"></div>
+            </div>
+        </a>
+        <a href="{{route('admin.members')}}">
+            <div>
+                <p>Membres</p>
+                <div class="on"></div>
+            </div>
+        </a>
+    </nav>
+    <div class="graphs">
+        <div class="graph">
+            {!! $chart->container() !!}
+        </div>
+    </div>
+
+
+
+
+
+
+    <!--<a href="{{ route('jeux.create') }}">Ajouter un jeu</a>
+    <a href="{{ route('consoles.create') }}">Ajouter une console</a>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.1/Chart.min.js" charset="utf-8"></script>
+    {!! $chart->script() !!}-->
+
+</body>
+</html> 
