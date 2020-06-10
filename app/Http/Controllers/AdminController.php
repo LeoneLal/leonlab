49cadeca56ee9e800e235b99181943143ac8f8f8
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-10 19:15:42 +0200

    Update AdminController.php
    Changements dans le graphique principal

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,21 +6,29 @@ use Illuminate\Http\Request;
 use App\Charts\SampleChart;
 use App\Jeu;
 use App\User;
+use App\Ligne;
 use Carbon\Carbon;
 
 
 class AdminController extends Controller
 {
     public function index(){
+        $nb_sales = Ligne::all()->count();
+        $nb_users = User::all()->count();
         $nb_jeux = Jeu::all()->count();
+        /**
+         * Data sur les 7 derniers jours
+         
         $last_users = User::where( 'created_at', '>', Carbon::now()->subDays(7))
            ->count();
-        $users = User::all()->count();
-        $color = '#DD9F80';
+        */
+        
         $chart = new SampleChart;
         $chart->title('Graphique global');
-        $chart->labels(['Nouveaux membres sur les 7 derniers jours', "Global"]);
-        $chart->dataset('Nb membres', 'bar', [$last_users, $users]);
+        $chart->labels(['Ventes totales', "Nombre de membres", 'Nombre de jeux']);
+        $chart->dataset('Quantités', 'bar', [$nb_sales, $nb_users, $nb_jeux])
+            ->color("#DFAEFF")
+            ->backgroundcolor("rgb(171, 235, 244)");
 
         return view('admin.index', compact('chart'));
     }
