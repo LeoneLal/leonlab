b7d4d469887ec443e9891f8f774d053ec75de088
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-12 19:02:34 +0200

    Update PdfController.php

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -6,6 +6,7 @@ use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
 use App\User;
+use App\Fiche;
 use PDF;
 
 class PdfController extends Controller
@@ -13,20 +14,20 @@ class PdfController extends Controller
     public function receipt(){
         $username = \Auth::user()->name;
         $mytime = Carbon::now();
-        return view('pdf.facture', compact('mytime', 'username'));
+        $fiche = Fiche::all()->last();
+        $number = $fiche->id;
+        return view('pdf.bill', compact('mytime', 'username', 'number'));
     }
 
     public function print(){
         $username = \Auth::user()->name;
         $mytime = Carbon::now();
-       /* $pdf = PDF::loadView('pdf.facture', compact('mytime', 'username'));
-        return $pdf->download("Facture.pdf");
-*/
-        
-        return PDF::loadView('pdf.facture', compact('mytime', 'username'))
+        $fiche = Fiche::all()->last();
+        $number = $fiche->id;
+        $pdf =  PDF::loadView('pdf.bill', compact('mytime', 'username', 'number'))
         ->setPaper('a4')
         ->setWarnings(false)
-        ->save(public_path("factures/fichier.pdf"))
-        ->stream();
-            }
+        ->save(public_path("factures/Facture$number.pdf"));
+        return redirect()->route('jeux.index');
+    }
 }
