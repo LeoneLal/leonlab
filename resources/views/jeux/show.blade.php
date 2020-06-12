fc96e05a025f746d603aa6babb3ed2e88d6c0907
AurianeLab <34133072+AurianeLab@users.noreply.github.com>
2020-06-12 17:05:49 +0200

    Update Jeu

--- resources/views/jeux/show.blade.php
+++ resources/views/jeux/show.blade.php
@@ -30,10 +30,10 @@
 
       <div class="top-center">
         <form method='GET' action="{{ route('jeux.search')}}" class="d-flex mr-3">
-            <input type="text" name="q" placeholder="Trouvez votre jeu !" />
-            <button type="submit" class="btn btn-info">
-                <i class="fas fa-search"></i>
-            </button>
+          <input type="text" name="q" placeholder="Trouvez votre jeu !" />
+          <button type="submit" class="btn btn-info">
+            <i class="fas fa-search"></i>
+          </button>
         </form>
       </div>
 
@@ -49,46 +49,54 @@
       </div>
       @endif
     </div>
+
     <div class="main">
-        <div class="one-game container">
-          <div class="row">
-            <img src="../../images/jeux/{{$jeu->slug}}" alt="{{ $jeu->nom }}" />
+      <div class="container">
+        <div class="row">
+          <img src="../../images/jeux/{{$jeu->slug}}" alt="{{ $jeu->nom }}" />
+          <div class="column center">
             <p><b>{{ $jeu->nom }}</b></p>
-            <p>{{ $jeu->console['console'] }}</p>
-            <p>{{ $jeu->prix }}€</p>
-          </div>
-          <div>
-            <div class="block">
-              <h3>Description</h3>
-              <p>{{ $jeu->description }}</p>
-            </div>
-            <div class="block">
-              <h3>Avis</h3>
-              <div class="comments">
-                <p>1</p>
-                <p>2</p>
-                <p>3</p>
-                <p>4</p>
+            <div class="consoles">
+              <div class="console">
+                <div class="logo">
+                  <img src="../../images/{{ $jeu->console['slug'] }}" alt="{{ $jeu->console['console'] }}" />
+                </div>
+                <p><b>{{ $jeu->console['console'] }}</b></p>
               </div>
             </div>
+            <p>{{ $jeu->prix }}€</p>
           </div>
-        </div>
-        <div class="panier">
-          <form action="{{ route('cart.store')}}" method="POST">
-            @csrf
-            <input type="hidden" name="product_id" value="{{ $jeu->id }}">
-            @if($jeu->stock > 0 && $jeu->stock <= 3)
-            <div class="alert alert-warning">
-              <p>Plus que {{ $jeu->stock }} jeux en stock !</p>
+          <div class="block">
+            <h3>Description</h3>
+            <p>{{ $jeu->description }}</p>
+          </div>
+          <div class="block">
+            <h3>Avis</h3>
+            <div class="comments">
+              <p>1</p>
+              <p>2</p>
+              <p>3</p>
+              <p>4</p>
             </div>
-            <button type="submit" class="btn btn-dark">Ajouter au panier</button>
-            @else
-            <div class="alert alert-danger">
-              <p>Plus en stock !</p>
-            </div>
-            @endif
-          </form>
-        </div>
+          </div>
+        </div> 
+      </div>
+      <div class="panier">
+        <form action="{{ route('cart.store')}}" method="POST">
+          @csrf
+          <input type="hidden" name="product_id" value="{{ $jeu->id }}">
+          @if($jeu->stock > 0 && $jeu->stock <= 3)
+          <div class="alert alert-warning">
+            <p>Plus que {{ $jeu->stock }} jeux en stock !</p>
+          </div>
+          <button type="submit" class="btn btn-dark">Ajouter au panier</button>
+          @else
+          <div class="alert alert-danger">
+            <p>Plus en stock !</p>
+          </div>
+          @endif
+        </form>
+      </div>
     </div>
     @extends('footer')
   </body>
