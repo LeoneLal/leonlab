6fc61ae34ba6c79e41effc5b37f46d26d6ad8c69
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-17 20:40:56 +0200

    Update CartController.php

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -67,41 +67,61 @@ class CartController extends Controller
         return back()->with('success', 'Le produit a été supprimé.');
     }
 
-
     public function pay(Request $request)
     {
+        /**
+         * Creation of the activation code of the game
+         */
+        $caracteres = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        
         $user_id = \Auth::user()->id;
+        /**
+         * Add of the cart to the database only if it's not empty
+         */
         if( Cart::count() > 0)
         {
             $username = \Auth::user()->name;
             $mytime = Carbon::now();
             $fiche = Fiche::all()->last();
-            $number = $fiche->id;
+            $number = Fiche::count() + 1;
             $pdf =  PDF::loadView('pdf.bill', compact('mytime', 'username', 'number'))
             ->setPaper('a4')
             ->setWarnings(false)
             ->save(public_path("factures/Facture$number.pdf"));
 
+            /**
+             * Adding the card to the BDD
+             */
             $fiche = new Fiche();
             $fiche->prix_total = Cart::subtotal();
             $fiche->facture = "Facture$number.pdf";
             $fiche->user_id = $user_id;
             $fiche->save();
 
-            
+            /**
+             * Adding each line of the card to the database
+             * Updating stocks
+             */
             foreach(Cart::content() as $jeu){
+                $chaineAleatoire = '';
+                for ($i = 0; $i < 10; $i++)
+                {
+                    $chaineAleatoire .= $caracteres[rand(0, strlen($caracteres) - 1)];
+                }
                 $ligne = new Ligne();
                 $ligne->fiche_id = $fiche->id;
                 $ligne->jeu_id = $jeu->model->id;
                 $ligne->prix = $jeu->model->prix;
-                $ligne->code = "azerty123";
+                $ligne->code = $chaineAleatoire;
                 $ligne->save();     
                 
                 $game = Jeu::where('id',$jeu->model->id)->first();
                 $game->stock = $jeu->model->stock - 1;
                 $game->save();
             }
-            
+            /**
+             * Updating user's balance
+             */
             $prec_solde = \Auth::user()->solde;
             $user = User::where('id', \Auth::user()->id)->first();
             $user->solde = $prec_solde - Cart::subtotal();
