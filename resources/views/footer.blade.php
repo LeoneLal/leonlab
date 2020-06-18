80a27e6c6b4b64b804e31d3e8e9e32276ed9fddf
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-18 13:06:57 +0200

    Update footer.blade.php

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,17 @@
 <div class="footer">
-    <img src="{{ asset('images/facebook.png') }}" alt="Facebook" class="social_media" />
-    <img src="{{ asset('images/instagram.png') }}" alt="Instagram" class="social_media" />
-    <img src="{{ asset('images/twitter.png') }}" alt="Twitter" class="social_media" />
+  <img
+    src="{{ asset('images/facebook.png') }}"
+    alt="Facebook"
+    class="social_media"
+  />
+  <img
+    src="{{ asset('images/instagram.png') }}"
+    alt="Instagram"
+    class="social_media"
+  />
+  <img
+    src="{{ asset('images/twitter.png') }}"
+    alt="Twitter"
+    class="social_media"
+  />
 </div>
