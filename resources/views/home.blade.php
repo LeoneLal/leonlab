4dfc18a108429a21fe5dc788ea80b4be010ac631
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-18 13:06:49 +0200

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,68 +3,66 @@
 @section('content')
 
 <div class="left">
-<div class="solde">
+  <div class="solde">
     <h2>Solde</h2>
     <p>{{ $user->solde }} €</p>
     <a href="{{ route('user.edit_solde') }}"><button>Modifier</button></a>
-</div>
+  </div>
 
-<div class="profil">
+  <div class="profil">
     <h2>Profil</h2>
     <div class="form">
-        <form  method="POST" action="{{ route('user.update_profil') }}">
+      <form method="POST" action="{{ route('user.update_profil') }}">
+        @csrf @method('PUT')
 
-        @csrf
-        @method('PUT')
+        <div class="champs">
+          <label>Nom</label>
+          <input type="text" name="name" value="{{ $user->name }}" />
+        </div>
 
-            <div class="champs">
-                <label>Nom</label>
-                <input type="text" name="name" value="{{ $user->name }}" />
-            </div>
+        <div class="champs">
+          <label>Mail</label>
+          <input type="email" name="email" value="{{ $user->email }}" />
+        </div>
 
-            <div class="champs">
-                <label>Mail</label>
-                <input type="email" name="email" value="{{ $user->email }}" />
-            </div>
+        <div class="champs">
+          <label>Nouveau mot de passe</label>
+          <input type="password" name="password" />
+        </div>
 
-            <div class="champs">
-                <label>Nouveau mot de passe</label>
-                <input type="password" name="password" />
-            </div>
+        <div class="champs">
+          <label>Vérification</label>
+          <input type="password" name="verifiation" />
+        </div>
 
-            <div class="champs">
-                <label>Vérification</label>
-                <input type="password" name="verifiation" />
-            </div>
-            
-            <button type="submit">Enregistrer</button>
-        </form>
+        <button type="submit">Enregistrer</button>
+      </form>
     </div>
-</div>
-
+  </div>
 </div>
 <div class="right">
-    <h2>Achats</h2>
-    <div class="jeux">
+  <h2>Achats</h2>
+  <div class="jeux">
     @foreach( $line as $game)
 
-        <div class="jeu">
-            <div class="picture">
-                <img src="images/jeux/{{$game->Game->slug}}" alt="Rust" />
-            </div>
-            <div class="infos">
-                <div class="titre">
-                    <h3>{{$game->Game->nom}}</h3>
-                </div>
-                <div class="date">
-                    <p>{{ $game->Card->created_at }}</p>
-                    <a href="{{ route('pdf.download', $game->Card->id) }}"><p><b>Facture</b><i class="fas fa-file-download"></i></p></a>
-                </div>
-            </div>
+    <div class="jeu">
+      <div class="picture">
+        <img src="images/jeux/{{$game->Game->slug}}" alt="Rust" />
+      </div>
+      <div class="infos">
+        <div class="titre">
+          <h3>{{$game->Game->nom}}</h3>
         </div>
-    @endforeach
-        
+        <div class="date">
+          <p>{{ $game->Card->created_at }}</p>
+          <a href="{{ route('pdf.download', $game->Card->id) }}"
+            ><p><b>Facture</b><i class="fas fa-file-download"></i></p
+          ></a>
+        </div>
+      </div>
     </div>
+    @endforeach
+  </div>
 </div>
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 @endsection
