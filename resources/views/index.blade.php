7951b6a35519081ed85052da4b98e1647fc6afd2
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-18 15:51:34 +0200

    Change footer organization

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -103,7 +103,23 @@
       </div>
       
     </div> 
-    @extends('footer')
+    <div class="footer">
+  <img
+    src="{{ asset('images/facebook.png') }}"
+    alt="Facebook"
+    class="social_media"
+  />
+  <img
+    src="{{ asset('images/instagram.png') }}"
+    alt="Instagram"
+    class="social_media"
+  />
+  <img
+    src="{{ asset('images/twitter.png') }}"
+    alt="Twitter"
+    class="social_media"
+  />
+</div>
     <nav class="menu">
         <div class="icons">
             <a href="index.html">
