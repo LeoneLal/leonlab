482c5e151e82fe525cc62649426f841858abbbf9
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-21 18:20:08 +0200

    Update email content

--- resources/views/mails/purchase.blade.php
+++ resources/views/mails/purchase.blade.php
@@ -1,9 +1,11 @@
-<h1>Votre facture est désormais disponible dans votre espace personnel</h1>
+<p>Bonjour,</p>
+
+<p>Votre facture est désormais disponible dans votre espace personnel</p>
 
 @foreach($codes as $code)
 <div class="game">
-<p>Voici le code d'activation du jeu <b>{{$code->Game->nom}}</b> : </p>
-
-<b>{{ $code->code }}</b>
+    <p>Voici le code d'activation du jeu <b>{{$code->Game->nom}}</b> : <b>{{ $code->code }}</b></p>
 </div>
 @endforeach
+
+<p>Cordialement l'équipe <a href="http://leonlab.herokuapp.com/">LéonLab</a> !</p>
