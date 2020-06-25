ef3c341e90caef49b681c3ec6b74709ac37c42b7
LeoneLal <45849538+LeoneLal@users.noreply.github.com>
2020-06-25 10:37:59 +0200

    Add condition to comment creation

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -12,20 +12,42 @@ class CommentsController extends Controller
 {
     public function create($gameId){
         $game = Jeu::where('id', $gameId)->first();
-        return view('comments.create', compact('game'));
+
+        $existing_comment = Comment::selectRaw('user_id , jeu_id')
+        ->where('user_id', \Auth::user()->id)
+        ->get();
+        $statut = false;
+
+        foreach($existing_comment as $comment){
+            if($comment->jeu_id == $gameId){
+                $statut = true;
+                break;
+            }
+            else{
+                $statut = false;
+            }
+        }
+        if($statut == false){
+            return view('comments.create', compact('game'));
+        }
+        else{
+            return redirect()->route('home');
+        }
+
+        
     }
     
     public function store(Request $request)
     {
+
         $comment = new Comment();
         $comment->user_id = \Auth::user()->id;
         $comment->jeu_id = $request->get('jeu');
         $comment->note = $request->get('note');
         $comment->avis = $request->get('avis');
         $comment->save();
-        if( \Auth::user()->role == 1)
-            return redirect()->route('home');
-        else
-            return view('index');
+            
+        return redirect()->route('home');
+    }
     }
 }
